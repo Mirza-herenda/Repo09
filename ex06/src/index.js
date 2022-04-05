8b6d63ede4ba291fb8c77cf933a250436dbea3cc
Mirza <herendamirza87@gmail.com>
2022-04-05 18:01:40 +0200

    using comparison to operator

---
+++ ex06/src/index.js
@@ -0,0 +1,26 @@
+function comparisonToEqual(a) 
+{
+    if(a<5)
+    return console.log("less than 5");
+
+    if(a<10)
+    return console.log("less than 10");
+
+    if(a<=20)
+    return console.log("10 or over");
+
+    if(a>20)
+    return console.log("More than 20"); 
+
+   
+
+   
+}
+comparisonToEqual(0);
+comparisonToEqual(5);
+comparisonToEqual(17);
+comparisonToEqual(21);
+
+
+
+module.exports=comparisonToEqual;
