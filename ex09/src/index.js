a400b2d98b6210eb396967ac0ee0cbf22e4a37a4
Mirza <herendamirza87@gmail.com>
2022-04-05 18:43:23 +0200

    adding || operator

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+
+function logicalOrOperator(num) 
+{
+ 
+    if( num<20 || num>30)  
+
+     return console.log("In");
+
+    return console.log("Out");
+}
+logicalOrOperator(0);
+logicalOrOperator(9);
+logicalOrOperator(20);
+ logicalOrOperator(23);
+ logicalOrOperator(30);
+ logicalOrOperator(31);
+ logicalOrOperator(105);
+
+module.exports=logicalOrOperator;
